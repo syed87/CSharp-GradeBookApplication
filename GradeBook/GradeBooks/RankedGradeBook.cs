b18c36df12f415029d581d5ecec8d31c8fbeecab
Syed Khalid <khalidshujat@gmail.com>
2021-11-11 09:28:49 +0000

    checking if answer given on youtube works

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,47 +16,60 @@ namespace GradeBook.GradeBooks
         //whichever averageGrade is passed as a parameter to the method GetLetterGrade you will get a corresponding grade for that value
         public override char GetLetterGrade(double averageGrade)
         {
-           //get the value of each grade and order
-           //find which 20% bracket the averageGrade input falls in the list
-           //return a corresponding grade
-          
             if (Students.Count < 5)
-                throw new InvalidOperationException();
-
-            var twentyPercent = Students.Count / 5;
-            
-            List<double> allGrades = new List<double>() { averageGrade };
-
-            if (allGrades.Count == Students.Count)
-            {
-                for (var i = 1; i <= 5; i++)
-                {
-                    var orderedGrades =
-                     from a in allGrades
-                     orderby a descending
-                     select a;
-                }
-            }
-
-            switch (gradeBook)
-            {
-                case 1:
-                    return 'A';
-                case 2:
-                    return 'B';
-                case 3:
-                    return 'C';
-                case 4:
-                    return 'D';
-                case 5:
-                    return 'E';
-            }
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            /////////////////////////////////////////////////
             
             
+            //var twentyPercent = Students.Count / 5;
 
             
+
+            //List<double> allGrades = new List<double>() { averageGrade };
+
+            //if (allGrades.Count == Students.Count)
+            //{
+            //    for (var i = 1; i <= 5; i++)
+            //    {
+            //        var orderedGrades =
+            //         from a in allGrades
+            //         orderby a descending
+            //         select a;
+            //    }
+            //}
+
+            //switch (gradeBook)
+            //{
+            //    case 1:
+            //        return 'A';
+            //    case 2:
+            //        return 'B';
+            //    case 3:
+            //        return 'C';
+            //    case 4:
+            //        return 'D';
+            //    case 5:
+            //        return 'F';
+            //}
             
+            
+            //////////////////////////////////
+            var threshold = (int)Math.Ceiling(Students * 0.2)
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[theshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(theshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(theshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(theshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
                 return 'F';
+
+
+
+
         }
     }
 }
